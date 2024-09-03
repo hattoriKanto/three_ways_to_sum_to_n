9d80c1001ee7292f30de8b0f462f264ceb7cad5c
Ihor Bahniuk <hattorikanto@gmail.com>
2024-09-04 00:54:26 +0300

    basic setup

---
+++ index.ts
@@ -0,0 +1,11 @@
+const sum_to_n_a = (n: number): number => {
+  return 0;
+};
+
+const sum_to_n_b = (n: number): number => {
+  return 0;
+};
+
+const sum_to_n_c = (n: number): number => {
+  return 0;
+};
