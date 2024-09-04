dbc61d8b2841e1e361559f44e2a38f067324dbed
Ihor Bahniuk <hattorikanto@gmail.com>
2024-09-04 19:35:19 +0300

    added comments

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+// Itteration approach.
+// Advantages: simple and uncomplicated approach.
+// Disadvantage: if there are much more iterations, it will lead to problems with optimisation.
 const sum_to_n_a = (n: number): number => {
   let result = 0;
 
@@ -12,6 +15,13 @@ const sum_to_n_a = (n: number): number => {
 
 sum_to_n_a(5);
 
+// Recursion approach.
+// Advantages:
+// - Great for complex tasks.
+// - Recursion is simple and straightforward (mostly time)
+// Disadvantages:
+// - If the base condition is not set correctly, an infinite loop can occur which will cause a freeze.
+// - Each function will be stored in the call stack, which may lead to stack overflow.
 const sum_to_n_b = (n: number): number => {
   const recurFunc = (n: number): number => {
     if (n === 0) {
@@ -29,6 +39,9 @@ const sum_to_n_b = (n: number): number => {
 
 sum_to_n_b(5);
 
+// For those who like maths (or know how to google formulas).
+// Advantages: it doesn't need a loop or recursion. Great for optimisation.
+// Disadvantages: it may take time to understand code with a lot of mathematical operations.
 const sum_to_n_c = (n: number): number => {
   const result = (n * (n + 1)) / 2;
   console.log("Result sum_to_n_c is:", result);
